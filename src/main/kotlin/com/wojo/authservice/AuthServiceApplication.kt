7fc71b23eaacb5c9c209027ece9c039c1cb8144d
Wojciech Kozioł <7wojok7@gmail.com>
2019-03-22 16:33:18 +0100

    cloud oauth2 dependency, enable authorization server

--- src/main/kotlin/com/wojo/authservice/AuthServiceApplication.kt
+++ src/main/kotlin/com/wojo/authservice/AuthServiceApplication.kt
@@ -2,8 +2,10 @@ package com.wojo.authservice
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer
 
 @SpringBootApplication
+@EnableAuthorizationServer
 class AuthServiceApplication
 
 fun main(args: Array<String>) {
