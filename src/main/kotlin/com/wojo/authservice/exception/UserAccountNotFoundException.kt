a3145e8c1b421ffa6b00547b48bd3add4bc1bfa0
Wojciech Kozioł <7wojok7@gmail.com>
2019-03-28 17:05:20 +0100

    Custom oauth exception serializer, refactor

--- src/main/kotlin/com/wojo/authservice/exception/UserAccountNotFoundException.kt
+++ src/main/kotlin/com/wojo/authservice/exception/UserAccountNotFoundException.kt
@@ -1,7 +1,6 @@
 package com.wojo.authservice.exception
 
 import org.springframework.http.HttpStatus
-import org.springframework.security.oauth2.common.exceptions.OAuth2Exception
 import org.springframework.web.bind.annotation.ResponseStatus
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
