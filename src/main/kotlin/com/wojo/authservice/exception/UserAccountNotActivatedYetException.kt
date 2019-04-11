02fa411847302e0712cca1649e5eb72fa230523d
Wojciech Kozioł <7wojok7@gmail.com>
2019-04-11 23:23:09 +0200

    Refactor

--- src/main/kotlin/com/wojo/authservice/exception/UserAccountNotActivatedYetException.kt
+++
@@ -1,8 +0,0 @@
-package com.wojo.authservice.exception
-
-import org.springframework.http.HttpStatus
-import org.springframework.web.bind.annotation.ResponseStatus
-
-@ResponseStatus(HttpStatus.UNAUTHORIZED)
-class UserAccountNotActivatedYetException(message: String) :
-        CustomOAuthException(message, ErrorCode.NOT_ACTIVATED_YET)
