270c672706b697948c260d220f1447d969fd52cb
Wojciech Kozioł <7wojok7@gmail.com>
2019-04-13 22:16:15 +0200

    Revert "Custom oauth exception add status"
    This reverts commit caa881bf

--- src/main/kotlin/com/wojo/authservice/exception/CustomOAuthException.kt
+++ src/main/kotlin/com/wojo/authservice/exception/CustomOAuthException.kt
@@ -1,12 +1,10 @@
 package com.wojo.authservice.exception
 
-import com.fasterxml.jackson.databind.annotation.JsonSerialize
 import org.springframework.http.HttpStatus
 import org.springframework.security.oauth2.common.exceptions.OAuth2Exception
 
 @JsonSerialize(using = CustomOauthExceptionSerializer::class)
 open class CustomOAuthException(
         message: String,
-        val errorCode: ErrorCode,
-        val status: HttpStatus
+        val errorCode: ErrorCode
 ) : OAuth2Exception(message)
--- src/main/kotlin/com/wojo/authservice/validation/status/strategies/DefaultStatusProcessor.kt
+++ src/main/kotlin/com/wojo/authservice/validation/status/strategies/DefaultStatusProcessor.kt
@@ -1,7 +1,6 @@
 package com.wojo.authservice.validation.status.strategies
 
 import com.wojo.authservice.entity.UserEntity
-import com.wojo.authservice.exception.impl.AccountCreateException
 import com.wojo.authservice.model.UserStatus
 import com.wojo.authservice.validation.status.UserStatusStrategy
 import org.springframework.stereotype.Component
@@ -12,7 +11,8 @@ class DefaultStatusProcessor : UserStatusStrategy {
 
     override fun getStrategyName(): String = UserStatus.DEFAULT.name
 
-    override fun validateStatus(user: UserEntity): Optional<UserEntity> =
-            throw AccountCreateException("Error occurred while account was created")
+    override fun validateStatus(user: UserEntity): Optional<UserEntity> {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 
 }
