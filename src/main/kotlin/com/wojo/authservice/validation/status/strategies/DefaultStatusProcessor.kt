6b2deda83adc19a3be896451db7e0e26cb1539f1
Wojciech Kozioł <7wojok7@gmail.com>
2019-04-13 22:22:13 +0200

    Default and Pending status processor impl

--- src/main/kotlin/com/wojo/authservice/validation/status/strategies/DefaultStatusProcessor.kt
+++ src/main/kotlin/com/wojo/authservice/validation/status/strategies/DefaultStatusProcessor.kt
@@ -1,6 +1,7 @@
 package com.wojo.authservice.validation.status.strategies
 
 import com.wojo.authservice.entity.UserEntity
+import com.wojo.authservice.exception.impl.AccountCreateException
 import com.wojo.authservice.model.UserStatus
 import com.wojo.authservice.validation.status.UserStatusStrategy
 import org.springframework.stereotype.Component
@@ -11,8 +12,7 @@ class DefaultStatusProcessor : UserStatusStrategy {
 
     override fun getStrategyName(): String = UserStatus.DEFAULT.name
 
-    override fun validateStatus(user: UserEntity): Optional<UserEntity> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    override fun validateStatus(user: UserEntity): Optional<UserEntity> =
+            throw AccountCreateException("Error occurred while account was created")
 
 }
