1330c95288e467478335487acf833a615cf33e3a
Wojciech Kozioł <7wojok7@gmail.com>
2019-04-13 22:18:21 +0200

    Custom oauth exception add status

--- src/main/kotlin/com/wojo/authservice/exception/CustomOAuthException.kt
+++ src/main/kotlin/com/wojo/authservice/exception/CustomOAuthException.kt
@@ -1,10 +1,12 @@
 package com.wojo.authservice.exception
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize
 import org.springframework.http.HttpStatus
 import org.springframework.security.oauth2.common.exceptions.OAuth2Exception
 
 @JsonSerialize(using = CustomOauthExceptionSerializer::class)
 open class CustomOAuthException(
         message: String,
-        val errorCode: ErrorCode
+        val errorCode: ErrorCode,
+        val status: HttpStatus
 ) : OAuth2Exception(message)
