0d1d0c877788a713dfefc4a509502375e6a0e396
Wojciech Kozioł <7wojok7@gmail.com>
2019-04-23 12:25:04 +0200

    Save and mapper user models

--- src/main/kotlin/com/wojo/authservice/service/impl/CustomUserDetailService.kt
+++ src/main/kotlin/com/wojo/authservice/service/impl/CustomUserDetailService.kt
@@ -4,6 +4,7 @@ import com.wojo.authservice.entity.UserEntity
 import com.wojo.authservice.model.CustomUserDetail
 import com.wojo.authservice.model.UserInput
 import com.wojo.authservice.model.UserResponse
+import com.wojo.authservice.model.UserStatus
 import com.wojo.authservice.repository.UserRepository
 import com.wojo.authservice.service.spec.PermissionService
 import com.wojo.authservice.service.spec.UserService
@@ -12,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.stereotype.Service
+import java.time.LocalDateTime
 
 @Service
 class CustomUserDetailService @Autowired constructor(
@@ -29,7 +31,35 @@ class CustomUserDetailService @Autowired constructor(
     }
 
     override fun createUser(userInput: UserInput): UserResponse {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        // TODO duplicate check
+        val user: UserEntity = mapInputToEntity(userInput)
+        val savedUser: UserEntity = userRepository.save(user)
+
+        return mapEntityToResponse(savedUser)
+    }
+
+    private val mapInputToEntity: (UserInput) -> UserEntity = { userInput ->
+        UserEntity.build {
+            code = generateUserCode()
+            email = userInput.email
+            password = userInput.password
+            userStatus = UserStatus.CREATED
+            nickname = userInput.nickName
+            createTime = LocalDateTime.now()
+        }
+    }
+
+    private fun generateUserCode(): Long {
+        return 10L
+    }
+
+    private val mapEntityToResponse: (UserEntity) -> UserResponse = { user ->
+        UserResponse.build {
+            id = user.id!! // TODO broken window
+            code = user.code
+            email = user.email
+            nickName = user.nickname
+        }
     }
 
 }
