0875428fbb768b96ff56058624aff86a8a89b0e8
Wojciech Kozioł <7wojok7@gmail.com>
2019-06-18 10:44:13 +0200

    Fix email validation

--- src/main/kotlin/com/wojo/authservice/model/UserInput.kt
+++ src/main/kotlin/com/wojo/authservice/model/UserInput.kt
@@ -1,12 +1,14 @@
 package com.wojo.authservice.model
 
-import javax.validation.constraints.Email
 import javax.validation.constraints.NotBlank
+import javax.validation.constraints.Pattern
 
 data class UserInput(
 
         @field:NotBlank
-        @field:Email
+        @field:Pattern(
+                message = "Email invalid",
+                regexp = "(([^<>()\\[\\]\\\\.,;:\\s@\"]+(\\.[^<>()\\[\\]\\\\.,;:\\s@\"]+)*)|(\".+\"))@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))\$")
         val email: String = "",
 
         @field:NotBlank
