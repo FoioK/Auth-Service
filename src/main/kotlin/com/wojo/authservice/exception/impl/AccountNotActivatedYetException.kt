de17cb04b82c87ca8873087f58e47a82637d07c1
Wojciech Kozioł <7wojok7@gmail.com>
2019-07-12 23:35:48 +0200

    Fix exception response statuses

--- src/main/kotlin/com/wojo/authservice/exception/impl/AccountNotActivatedYetException.kt
+++ src/main/kotlin/com/wojo/authservice/exception/impl/AccountNotActivatedYetException.kt
@@ -3,6 +3,8 @@ package com.wojo.authservice.exception.impl
 import com.wojo.authservice.exception.CustomOAuthException
 import com.wojo.authservice.exception.ErrorCode
 import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.ResponseStatus
 
+@ResponseStatus(HttpStatus.UNAUTHORIZED)
 class AccountNotActivatedYetException(message: String) :
         CustomOAuthException(message, ErrorCode.NOT_ACTIVATED_YET, HttpStatus.UNAUTHORIZED)
