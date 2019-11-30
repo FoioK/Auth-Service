49e88c803f70784b9a7ddec74eaacb118ac3b7c9
Wojciech Kozioł <7wojok7@gmail.com>
2019-11-30 22:10:32 +0100

    Update models

--- src/main/kotlin/com/wojo/authservice/model/UserInput.kt
+++ src/main/kotlin/com/wojo/authservice/model/UserInput.kt
@@ -16,24 +16,5 @@ data class UserInput(
 
         @field:NotBlank
         val username: String = ""
-) {
 
-    private constructor(builder: Builder) : this(
-            builder.email,
-            builder.password,
-            builder.username
-    )
-
-    companion object {
-        inline fun build(block: Builder.() -> Unit) = Builder().apply(block).build()
-    }
-
-    class Builder {
-
-        var email: String = ""
-        var password: String = ""
-        var username: String = ""
-
-        fun build() = UserInput(this)
-    }
-}
+)
